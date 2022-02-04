19090641af6a65b4958e7f4d37757d6064c8ef2f
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-02-04 14:17:45 +0500

    Install tailwindcss postcss autoprefixer

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './assets/**/*.{css}',
+    './components/*.{vue,js}',
+    './components/**/*.{vue,js}',
+    './pages/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './*.{vue,js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
